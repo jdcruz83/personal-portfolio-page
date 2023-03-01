fa04d588de8b1252f2c183a3a0be3bf32d701258
jdcruz83 <jdcruz83@hotmail.com>
2023-03-01 23:55:01 +0000

    new version

---
+++ v2/README.md
@@ -0,0 +1,4 @@
+# Personal Portfolio Page
+
+This is my source code of my portfolio page. I use it with continuous deployment with netlify.
+jdcruz83
---
+++ v2/js/app.js
@@ -0,0 +1,8 @@
+const sections = document.querySelectorAll(".section");
+const sectionBtns = document.querySelectorAll(".controls");
+const sectionBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransition() {
+  // Activate button on click
+}
